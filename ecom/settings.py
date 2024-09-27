c70ca50de2a13c53d090e752507f52124ea16729
Anandhurai <anand2004d@gmail.com>
2024-09-27 04:43:52 -0700

    final

--- ecom/settings.py
+++ ecom/settings.py
@@ -99,12 +99,15 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'myDatabase',
+            'USER': 'myProc',
+            'PASSWORD': 'user123',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
